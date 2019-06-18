cc1223e2735425785386ea8c9f864ec11b0d64b6
Bennie <brobinson88@gmail.com>
2019-06-18 10:30:16 -0400

    Minor cleanup

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,7 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
-const getUri = (admin, password, server) => `mongodb+srv://${admin}:${password}@${server}`;
+const getUri = (admin, password, server) =>
+  `mongodb+srv://${admin}:${password}@${server}`;
 
 const connect = async (admin, password, server) => {
   const uri = getUri(admin, password, server);
